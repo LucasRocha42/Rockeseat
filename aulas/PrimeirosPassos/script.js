4ee66a949f4a733c0a6d51044e42008e203476d2
lucasrocha42 <lucasfariascr1@gmail.com>
2022-03-30 11:23:59 -0300

    jk

--- aulas/PrimeirosPassos/script.js
+++ aulas/PrimeirosPassos/script.js
@@ -1,9 +1,2 @@
-function parimp(n){
-    if (n % 2== 0){
-        return 'par'
-    }else{
-        return 'impar'
-    }
-}
-let res = parimp(3)
-console.log(res)
+let string = "458"
+console.log (string.toLocaleLowerCase())
