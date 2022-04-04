df18521f535df00bdf615528188c2bf156317c3d
lucasrocha42 <lucasfariascr1@gmail.com>
2022-04-04 09:20:56 -0300

    ok

--- aulas/PrimeirosPassos/script.js
+++ aulas/PrimeirosPassos/script.js
@@ -1,2 +1,7 @@
-let string = "458"
-console.log (string.toLocaleLowerCase())
+let temperature = 36.9 
+let highTemperature =  temperature > 37.5 
+let mediumTemperature = temperature >= 36.7 && 38.8; 
+
+if ( highTemperature) {
+    console.log('Febre alta')
+}
