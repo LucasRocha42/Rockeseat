409ba8c7fdaaf640481d877c19cdf0c44a517db5
lucasrocha42 <lucasfariascr1@gmail.com>
2022-04-05 10:14:26 -0300

    There

--- aulas/PrimeirosPassos/script.js
+++ aulas/PrimeirosPassos/script.js
@@ -1,15 +1,35 @@
 
 
-let expression = 1
 
-switch(expression){
-    case 1:
-        console.log('Olá')
-    break
-    case 2: 
-        console.log('2')
-    break
-    default:
-        console.log('ruim')
-        break
+
+
+
+function getScore(score){
+
+let scoreFinal;
+
+let scoreA = score >= 90 && score <= 100 
+let scoreB = score >= 80 && score <= 90
+let scoreC = score >= 70 && score <= 80
+let scoreD = score >= 60 && score <= 70
+let scoreF = score < 60 && score >= 0  
+ 
+if (scoreA){
+    scoreFinal = 'A'
+} else if (scoreB){
+    scoreFinal = 'B'
+} else if (scoreC){
+    scoreFinal = 'C'
+} else if ( scoreD){
+    scoreFinal = 'D'
+} else if (scoreF){
+    scoreFinal = 'F'
 }
+return scoreFinal
+
+}
+console.log(getScore(90))
+console.log(getScore(80))
+console.log(getScore(70))
+console.log(getScore(60))
+console.log(getScore(50))
