ed20f5fe2acd93e7378330a1ffd0a267df8f1470
lucasrocha42 <lucasfariascr1@gmail.com>
2022-04-06 10:38:14 -0300

    transform

--- aulas/PrimeirosPassos/celsius copy.js
+++ aulas/PrimeirosPassos/celsius copy.js
@@ -1,34 +1,35 @@
 
-
-
-
-
 function transformDegree(degree) {
+    //transformar em maiúsculo a abreviação do grau
     const celsiusExists = degree.toUpperCase().includes('C')
     const fahrenheitExist = degree.toUpperCase().includes('F')
+
 
     // fluxo de erro
     if(!celsiusExists && !fahrenheitExist){
         throw new Error('Grau não identificado')
     }
 
-    // Fluxo ideal 
+
+    // fluxo ideal F to C
     let updateDegree = Number(degree.toUpperCase().replace("F", ""));
     let formula = fahrenheit => (fahrenheit - 32) * 5/9
     let degreeSign = 'C'
 
-    if (celsiusExists){
+        // fluxo alternativo C to F 
+        if(celsiusExists){
         updateDegree = Number(degree.toUpperCase().replace("C", ""));
         formula = celsius => celsius * 9/5 + 32
         degreeSign = 'F'
-    }
+        }
 
     return formula(updateDegree) + degreeSign
 }
+
 try{
     console.log(transformDegree('50F'))
     console.log(transformDegree('10C'))
-    /* transformDegree('20Z') */
+    transformDegree('20Z')
 }
 catch(error){
      console.log(error.message)
