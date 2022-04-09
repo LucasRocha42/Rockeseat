a721ab29b8429fcbf785c6e75538c209c0841c61
lucasrocha42 <lucasfariascr1@gmail.com>
2022-04-09 07:55:07 -0300

    celsius to f

--- aulas/PrimeirosPassos/celsius.js
+++ aulas/PrimeirosPassos/celsius.js
@@ -3,6 +3,46 @@
 
 
 
+
+
+
+
+
+
+
+function transFormDegree (degree){
+    const celsiusExists = degree.toUpperCase().includes('C')
+    const fahrenheitExists = degree.toUpperCase().includes('F')
+
+    if (!celsiusExists && !fahrenheitExists){
+        throw new Error ("Errou")
+    }
+
+    // Fluxo ideal 
+    let updateDegree = Number(degree.toUpperCase().replace('F',''))
+    let formula = fahrenheit => (fahrenheit - 32) * 5/9   
+    let degreeSign = "C"
+
+    if (celsiusExists){
+        updateDegree = Number(degree.toUpperCase().replace('C',''))
+        forumula = celsius => celsius * 9/5 + 32 
+        degreeSign = "F"
+    }
+
+    return formula(updateDegree) + degreeSign
+
+}
+try{
+    console.log(transFormDegree("80F"))
+    console.log(transFormDegree("40C"))
+    console.log(transFormDegree("5646"))
+}
+catch (error){
+    console.log(error)
+}
+
+
+/*
 function transformDegree(degree) {
     const celsiusExists = degree.toUpperCase().includes('C')
     const fahrenheitExist = degree.toUpperCase().includes('F')
@@ -29,7 +69,7 @@ try{
     console.log(transformDegree('50F'))
     console.log(transformDegree('10C'))
     /* transformDegree('20Z') */
-}
+/* }
 catch(error){
      console.log(error.message)
-    }
+    }  */
