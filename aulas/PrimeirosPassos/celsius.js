3f5ec4459ce409d69b05f58776d9f595404aa719
lucasrocha42 <lucasfariascr1@gmail.com>
2022-04-13 08:51:57 -0300

    celsius

--- aulas/PrimeirosPassos/celsius.js
+++ aulas/PrimeirosPassos/celsius.js
@@ -10,39 +10,40 @@
 
 
 
-function transFormDegree (degree){
+
+
+function trasformeDegree (degree){
+    // Create two variable for soon input in flow
+    const fahrenheitExist = degree.toUpperCase().includes('F')
     const celsiusExists = degree.toUpperCase().includes('C')
-    const fahrenheitExists = degree.toUpperCase().includes('F')
-
-    if (!celsiusExists && !fahrenheitExists){
-        throw new Error ("Errou")
+    // Create of flow case to error]
+    if(!fahrenheitExist && !celsiusExists){// Se celsius e fahrenheit não existirem 
+    throw new Error ('Grau não identificado')
     }
-
-    // Fluxo ideal 
-    let updateDegree = Number(degree.toUpperCase().replace('F',''))
-    let formula = fahrenheit => (fahrenheit - 32) * 5/9   
+    // flow ideal for fahreinht 
+    let updateDegree = Number(degree.toUpperCase().replace("F", ""));
+    let formula = fahrenheit => (fahrenheit - 32) + 5/9 
     let degreeSign = "C"
-
-    if (celsiusExists){
-        updateDegree = Number(degree.toUpperCase().replace('C',''))
-        forumula = celsius => celsius * 9/5 + 32 
-        degreeSign = "F"
+    // control of flow
+    if(celsiusExists){
+     uptadeDegree = Number(degree.toUpperCase().replace("C",""))
+     formula = celsius => celsius * 9/5 + 32
+     degreeSign = "F"
     }
-
     return formula(updateDegree) + degreeSign
-
 }
+
 try{
-    console.log(transFormDegree("80F"))
-    console.log(transFormDegree("40C"))
-    console.log(transFormDegree("5646"))
+    console.log(trasformeDegree('50F'))
+    console.log(trasformeDegree('10C'))
+    trasformeDegree('20Z') 
 }
-catch (error){
-    console.log(error)
-}
+ catch(error){
+     console.log(error.message)
+    }  
+  
+/* 
 
-
-/*
 function transformDegree(degree) {
     const celsiusExists = degree.toUpperCase().includes('C')
     const fahrenheitExist = degree.toUpperCase().includes('F')
@@ -68,8 +69,9 @@ function transformDegree(degree) {
 try{
     console.log(transformDegree('50F'))
     console.log(transformDegree('10C'))
-    /* transformDegree('20Z') */
-/* }
-catch(error){
+ /* transformDegree('20Z') */
+ 
+/* catch(error){
      console.log(error.message)
     }  */
+    
